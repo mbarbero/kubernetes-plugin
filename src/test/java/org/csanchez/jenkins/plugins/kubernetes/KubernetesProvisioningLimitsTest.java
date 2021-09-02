a77068cfcae9c0b24ddb109d501f5ea1b004c62c
Mikaël Barbero <mikael.barbero@gmail.com>
2021-09-02 12:10:39 +0200

    Tentative fix of a race condition in KubernetesProvisioningLimits

--- src/test/java/org/csanchez/jenkins/plugins/kubernetes/KubernetesProvisioningLimitsTest.java
+++ src/test/java/org/csanchez/jenkins/plugins/kubernetes/KubernetesProvisioningLimitsTest.java
@@ -1,21 +1,24 @@
 package org.csanchez.jenkins.plugins.kubernetes;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.LoggerRule;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.logging.Level;
-
-import static org.junit.Assert.assertEquals;
-
 public class KubernetesProvisioningLimitsTest {
+
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
@@ -25,40 +28,51 @@ public class KubernetesProvisioningLimitsTest {
     @Test
     public void lotsOfCloudsAndTemplates() throws InterruptedException {
         ThreadLocalRandom testRandom = ThreadLocalRandom.current();
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i < 4; i++) {
             KubernetesCloud cloud = new KubernetesCloud("kubernetes-" + i);
-            cloud.setContainerCap(testRandom.nextInt(40)+1);
-            for (int j = 1; j < 10; j++) {
+            cloud.setContainerCap(testRandom.nextInt(4)+1);
+            for (int j = 1; j < 4; j++) {
                 PodTemplate pt = new PodTemplate();
+                pt.setName(cloud.name + "-podTemplate-" + j);
                 pt.setInstanceCap(testRandom.nextInt(4)+1);
                 cloud.addTemplate(pt);
             }
             j.jenkins.clouds.add(cloud);
         }
-        ExecutorService EXEC = Executors.newFixedThreadPool(100);
-        List<Callable<Void>> tasks = new ArrayList<>();
-        for (int k = 0 ; k < 1000; k++) {
-            Callable<Void> c = () -> {
+
+        ExecutorService threadPool = Executors.newWorkStealingPool(Runtime.getRuntime().availableProcessors()*4);
+        CompletionService<Void> ecs = new ExecutorCompletionService<>(threadPool);
+        KubernetesProvisioningLimits kubernetesProvisioningLimits = KubernetesProvisioningLimits.get();
+
+        List<KubernetesCloud> clouds = j.jenkins.clouds.getAll(KubernetesCloud.class);
+        for (int k = 0 ; k < 100000; k++) {
+            ecs.submit(() -> {
                 ThreadLocalRandom random = ThreadLocalRandom.current();
-                List<KubernetesCloud> clouds = j.jenkins.clouds.getAll(KubernetesCloud.class);
                 KubernetesCloud cloud = clouds.get(random.nextInt(clouds.size()));
                 List<PodTemplate> templates = cloud.getTemplates();
                 PodTemplate podTemplate = templates.get(random.nextInt(templates.size()));
-                if (KubernetesProvisioningLimits.get().register(cloud, podTemplate, 1)) {
-                    Thread.sleep(random.nextInt(3) * 1000);
-                    KubernetesProvisioningLimits.get().unregister(cloud, podTemplate, 1);
+                while (!kubernetesProvisioningLimits.register(cloud, podTemplate, 1)) {
+                    Thread.yield();
                 }
-                return null;
-            };
-            tasks.add(c);
+
+                ecs.submit(() -> {
+                    kubernetesProvisioningLimits.unregister(cloud, podTemplate, 1);
+                }, null);
+            }, null);
         }
-        EXEC.invokeAll(tasks);
+
+        while (ecs.poll(2, TimeUnit.SECONDS) != null) {
+            Thread.yield();
+        }
+
+        threadPool.shutdown();
+        assertTrue(threadPool.awaitTermination(10, TimeUnit.SECONDS));
 
         // Check that every count is back to 0
         for (KubernetesCloud cloud : j.jenkins.clouds.getAll(KubernetesCloud.class)) {
-            assertEquals(0, KubernetesProvisioningLimits.get().getGlobalCount(cloud.name).get());
+            assertEquals(0, KubernetesProvisioningLimits.get().getGlobalCount(cloud.name));
             for (PodTemplate template : cloud.getTemplates()) {
-                assertEquals(0, KubernetesProvisioningLimits.get().getPodTemplateCount(template.getId()).get());
+                assertEquals(0, KubernetesProvisioningLimits.get().getPodTemplateCount(template.getId()));
             }
         }
     }
